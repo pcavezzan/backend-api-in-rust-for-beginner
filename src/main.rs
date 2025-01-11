e6961787c0ea04d9856cd8fc20f86875e49dd2cb
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-01-11 16:38:27 +0100

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, routes};
+
+#[get("/")]
+async fn hello() -> &'static str {
+    "Hello, world!\n"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+    .mount("/", routes![hello])
+    .launch()
+    .await;
 }
