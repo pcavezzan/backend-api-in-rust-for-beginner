3d08520dc686dfdb79f7b3188bda352d30a2fb9f
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-02-21 08:56:28 +0100

    Reverse proxying with nginx and ssl.

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,3 +4,7 @@ cp ./sytemd.service /etc/systemd/system/backend-api-in-rust-for-beginner.service
 systemctl start backend-api-in-rust-for-beginner
 # Make app starts whenever our OS starts
 systemctl enable backend-api-in-rust-for-beginner
+# Enable nginx site for backend-api-in-rust-for-beginner
+cp ./nginx.conf /etc/nginx/sites-available/backend-api-in-rust-for-beginner
+# Make nginx read the new configuration for our app to make it available
+systemctl reload nginx
